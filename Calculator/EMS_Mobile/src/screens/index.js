766214d099c82002cab23fc568fcf3fb04e888e4
in4tech <77341814+in4tech@users.noreply.github.com>
2021-04-16 19:02:14 +0700

    Calculator_AltaMedia_Test

---
+++ Calculator/EMS_Mobile/src/screens/index.js
@@ -0,0 +1,40 @@
+import Notification from './Main/Notification';
+import NotificationDetail from './Main/NotificationDetail';
+import AdDetails from './Main/AdDetails';
+import ByBankCard from './Main/ByBankCard';
+import ByCash from './Main/ByCash';
+import Complain from './Main/Complain';
+import Payment from './Main/Payment';
+import PaymentDetail from './Main/PaymentDetail';
+import PaymentFailed from './Main/PaymentFailed';
+import PaymentMethods from './Main/PaymentMethods';
+import PaymentSuccess from './Main/PaymentSuccess';
+import PrmDetails from './Main/PrmDetail';
+import Profile from './Main/Profile';
+import ProfileDetail from './Main/ProfileDetail';
+import Search from './Main/Search';
+import QRcode from './Auth/QRcode';
+import Login from './Auth/Login';
+
+import Home from './Main/Home';
+
+export {
+    Notification,
+    NotificationDetail,
+    AdDetails,
+    ByBankCard,
+    ByCash,
+    Complain,
+    Login,
+    Payment,
+    PaymentDetail,
+    PaymentFailed,
+    PaymentSuccess,
+    PaymentMethods,
+    PrmDetails,
+    Profile,
+    ProfileDetail,
+    QRcode,
+    Search,
+    Home,
+}
